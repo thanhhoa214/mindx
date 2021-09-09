1a15975c9b25f85b06ab8a4009e5ee9b261d8765
thanhhoa214 <hoa.nguyen@nng.bz>
2021-09-09 21:13:32 +0700

    initial commit

---
+++ apps/world-population/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'xmind-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'world-population';
+}
